cb676d8ccb298c8921e3e868473bef4c03955225
andrev <andrev@PC-B102-03.iut-valence.fr>
2014-02-12 16:36:59 +0100

    Bhou ! j'ai modifier quelque peu le main !!

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Lampe maLampe = new Lampe() ;
 		System.out.println("la puissance est de: " + maLampe.getPuissance());
-		System.out.println(maLampe.getestAllumée());
+		System.out.println("La lampe est dans un état: " + maLampe.getestAllumée());
 
 	}
 	
